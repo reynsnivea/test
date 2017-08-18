b6937f047fef7c820e9c923be4310b376ee92089
reynsnivea <reynsnivea@gmail.com>
2017-08-18 09:53:04 +0200

    cherry-picked

--- test_git.R
+++ test_git.R
@@ -16,4 +16,11 @@ write.csv
 
 #add production working directory here
 
+<<<<<<< HEAD
 setwd("c:\production")
+=======
+#This is working and i want to add it to the production
+
+  "this is working"
+
+>>>>>>> e3a18c6... working feature
